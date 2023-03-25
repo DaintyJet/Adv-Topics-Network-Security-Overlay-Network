0049c61b78933048e634fc71785afae2590b440e
AnirudhSunil <112992046+AnirudhSunil@users.noreply.github.com>
2023-03-25 14:35:19 -0400

    Update network.py

--- Source/Network/network.py
+++ Source/Network/network.py
@@ -1,28 +1,35 @@
-# This is a Placeholder
-
-# Import library for timestamps 
-from datetime import datetime
-# Import socket for network functionality
 import socket
-# Import the jason library for serializing and deserializing data
-import json
+import threading
+import time
 
-from threading import *
+# Server code
+class Server:
+    def __init__(self, port):
+        self.port = port
+        self.clients = {}                                   #storing the clients
+        self.lock = threading.Lock()
 
-netsoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-netsoc.bind(("0.0.0.0", 9999))
+    def start(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('localhost', self.port))
+        self.sock.listen(5)
 
+        print(f'Server listening on port {self.port}')
 
-def test(clientSoc):
-    # Debug
-    print("Here")
-    mssg = clientSoc.recv(2048).decode("utf8")
-    print(mssg)
-    clientSoc.send(json.dumps(["TEST", "127.0.0.1"]).encode("utf8"))
-    clientSoc.close()
+        while True:
+            conn, addr = self.sock.accept()
+            print(f'New client connected: {addr}')
+            threading.Thread(target=self.handle_client, args=(conn,)).start()
 
-while 1:
-    netsoc.listen(10)
-    clientSoc, clientAddr = netsoc.accept()
-    Thread(target=test,args=(clientSoc,)).start()
-    # Parse the flow
+    def handle_client(self, conn):
+        name = conn.recv(1024).decode()
+        self.clients[name] = conn
+
+        while True:
+            time.sleep(10) # Flow 2
+            conn.sendall(str(self.clients.keys()).encode())
+
+            
+if __name__ == '__main__':
+    server = Server(9999)
+    server.start()
